b0b647a2befa6e45f129fbd785429547adae5ec1
fuckshitter <90358434+fuckshitter@users.noreply.github.com>
2021-10-01 19:21:03 -0600

    sdf

---
+++ app.nftrade.com/js/analytics.js
@@ -0,0 +1,17 @@
+window.dataLayer = window.dataLayer || [];
+function gtag() { dataLayer.push(arguments); }
+gtag('js', new Date());
+
+gtag('config', window.GA_TRACKING_ID, {
+  page_path: window.location.pathname,
+});
+
+/* // Hotjar
+(function(h,o,t,j,a,r){
+  h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+  h._hjSettings={hjid:1565275,hjsv:6};
+  a=o.getElementsByTagName('head')[0];
+  r=o.createElement('script');r.async=1;
+  r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+  a.appendChild(r);
+})(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv='); */
